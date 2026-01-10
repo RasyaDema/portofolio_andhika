1372d741c8acb6f265682ba89263cda46b18e17e
Rasya Febrian Dema <rasyadema07@gmail.com>
2026-01-11 02:04:17 +0800

    memperbarui vercel

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "490371085"
+    serviceWorkerVersion: "1368072078"
   }
 });
